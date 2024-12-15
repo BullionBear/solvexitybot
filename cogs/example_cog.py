b706baf633b4255574b642891095a0cffe8b76b1
yite <yite@icloud.com>
2024-12-15 22:17:38 +0800

    simplify format

--- cogs/example_cog.py
+++ cogs/example_cog.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+import logging
+
+logger = logging.getLogger(__name__)
 
 class ExampleCog(commands.Cog):
     def __init__(self, bot):
@@ -8,12 +11,10 @@ class ExampleCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f"Example cog loaded")
+        logger.info(f"Example cog loaded")
 
     @app_commands.command(name="ping", description="Replies with 'Pong!'")
     async def ping(self,interaction: discord.Interaction):
         """Responds with a greeting"""
         await interaction.response.send_message("Pong!")
 
-async def setup(bot):
-    await bot.add_cog(ExampleCog(bot))
--- cogs/solvexity_cog.py
+++ cogs/solvexity_cog.py
@@ -28,7 +28,8 @@ class SolvexityDataCog(commands.Cog):
             if api_key["exchange"].values[0] == 'BINANCE':
                 # await interaction.response.send_message(f"Invalid API key")
                 df = pd.read_sql_query(f"""
-                                        SELECT dt, token, amount, value_in_usd FROM spot_balance 
+                                        SELECT TO_CHAR(dt, 'HH24:MI:SS') AS dt, 
+                                               token, amount, value_in_usd FROM spot_balance 
                                         WHERE account = '{account}'
                                         AND sid = (
                                             SELECT MAX(sid)
@@ -36,6 +37,7 @@ class SolvexityDataCog(commands.Cog):
                                             WHERE account = '{account}'
                                         )
                                         """, self.engine)
+                df.sort_values(by="token", ascending=True, inplace=True)
                 table = tabulate(df, headers="keys", tablefmt="github", showindex=False)
                 # Wrap the table in a code block for Discord
                 formatted_table = f"```\n{table}\n```"
@@ -49,7 +51,8 @@ class SolvexityDataCog(commands.Cog):
             elif api_key["exchange"].values[0] == 'BINANCEFUTURE':
                 # await interaction.response.send_message(f"Invalid API key")
                 df = pd.read_sql_query(f"""
-                                        SELECT dt, token, amount, value_in_usd FROM perp_balance 
+                                        SELECT TO_CHAR(dt, 'HH24:MI:SS') AS dt, 
+                                               token, amount, value_in_usd FROM perp_balance 
                                         WHERE account = '{account}'
                                         AND sid = (
                                             SELECT MAX(sid)
@@ -63,6 +66,43 @@ class SolvexityDataCog(commands.Cog):
                 embed = discord.Embed(
                     title=f"PERP {account} balance in USD",
                     description=f"Value in USD {df['value_in_usd'].sum():.2f}",
+                    color=discord.Color.green()
+                )
+                await interaction.response.send_message(formatted_table, embed=embed) 
+            else:
+                await interaction.response.send_message(f"{api_key['exchange']} is not implemented")
+        except Exception as e:
+            logger.error("Error handling /balance command", exc_info=True)
+            await interaction.response.send_message(f"Failed to get balance: {e}")
+
+    @app_commands.command(name="position", description="Get perp positions")
+    async def position(self, interaction: discord.Interaction, account: str):
+        """Responds with a position"""
+        try:
+            api_key = pd.read_sql_query(f"select * from read_api where account = '{account}' limit 1", self.engine)
+            if len(api_key) == 0:
+                await interaction.response.send_message(f"Account {account} not found")
+                return
+            if api_key["exchange"].values[0] == 'BINANCE':
+                await interaction.response.send_message("BINANCE account has no positions, try a BINANCEFUTURE account")
+
+            elif api_key["exchange"].values[0] == 'BINANCEFUTURE':
+                # await interaction.response.send_message(f"Invalid API key")
+                df = pd.read_sql_query(f"""
+                                        SELECT TO_CHAR(dt, 'HH24:MI:SS') AS dt, 
+                                               symbol, amount, unrealized_profit AS unrealized_pnl, 
+                                               entry_px, mark_px 
+                                        FROM position
+                                        WHERE account = '{account}'
+                                          AND dt >= NOW() - INTERVAL '1 hour';
+                                        """, self.engine)
+                df.sort_values(by="symbol", ascending=True, inplace=True)
+                table = tabulate(df, headers="keys", tablefmt="github", showindex=False)
+                # Wrap the table in a code block for Discord
+                formatted_table = f"```\n{table}\n```"
+                embed = discord.Embed(
+                    title=f"PERP {account} positions",
+                    description=f"Unrealized profit is in USD: {df['unrealized_pnl'].sum():.2f}",
                     color=discord.Color.green()
                 )
                 await interaction.response.send_message(formatted_table, embed=embed) 
